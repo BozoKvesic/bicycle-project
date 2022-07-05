e312c67cc7512a4b37d31df9d0117ee5642fb9e2
Bozo Kvesic <bozo.kvesic@ibmix.hr>
2022-07-05 14:04:45 +0200

    10 - removed dateOfPurchase param

--- src/main/java/com/spring/boot/kickstart/bicycleproject/controller/BillController.java
+++ src/main/java/com/spring/boot/kickstart/bicycleproject/controller/BillController.java
@@ -1,12 +1,9 @@
 package com.spring.boot.kickstart.bicycleproject.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.boot.kickstart.bicycleproject.entity.Bill;
@@ -19,7 +16,7 @@ public class BillController {
     private BillFacade billFacade;
 
     @GetMapping("/bills")
-    List<Bill> allBills(@RequestParam(value = "dateOfPurchase", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate dateOfPurchase) {
+    List<Bill> allBills() {
         return billFacade.findAll();
     }
 
