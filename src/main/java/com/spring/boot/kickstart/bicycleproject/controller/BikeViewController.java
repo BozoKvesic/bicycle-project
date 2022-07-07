f5bd6e85400c29bfaec803a0385fe0fc31c16fb9
Bozo Kvesic <bozo.kvesic@ibmix.hr>
2022-07-07 15:22:27 +0200

    15 - added FE for bill list

--- src/main/java/com/spring/boot/kickstart/bicycleproject/controller/BikeViewController.java
+++ src/main/java/com/spring/boot/kickstart/bicycleproject/controller/BikeViewController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.boot.kickstart.bicycleproject.facade.BikeFacade;
+import com.spring.boot.kickstart.bicycleproject.facade.BillFacade;
 
 @Controller
 public class BikeViewController {
@@ -14,10 +15,19 @@ public class BikeViewController {
     @Autowired
     private BikeFacade bikeFacade;
 
+    @Autowired
+    private BillFacade billFacade;
+
     @RequestMapping(value = "/getAllBikes", method = RequestMethod.GET)
     public String getAllBikes(final Model model) {
         model.addAttribute("bikes", bikeFacade.getAllBikes());
         return "bike";
+    }
+
+    @RequestMapping(value = "/getAllBills", method = RequestMethod.GET)
+    public String getAllBills(final Model model) {
+        model.addAttribute("bills", billFacade.findAllBills());
+        return "bill";
     }
 
 }
